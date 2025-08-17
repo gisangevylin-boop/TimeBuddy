08e126e9257a19b4511e1418c46544db98ef6483
gisangevylin-boop <gisangevylin@gmail.com>
2025-08-17 21:13:31 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
-// Replace with your actual Firebase config from console
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC5N0pzxGjxNUnFxJlZbkpXvPSkqg5jGdw",
+  authDomain: "timebuddy-d54a3.firebaseapp.com",
+  projectId: "timebuddy-d54a3",
+  storageBucket: "timebuddy-d54a3.firebasestorage.app",
+  messagingSenderId: "235396426991",
+  appId: "1:235396426991:web:c97868da076cd0f58abf1a",
+  measurementId: "G-KF4YRMJ6YE"
 };
 
 // Initialize Firebase
